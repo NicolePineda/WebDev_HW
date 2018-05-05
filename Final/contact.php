ad37302f59dc61949c19e8a6eafa5bd9f761927a
NicolePineda <np1779@nyu.edu>
2018-05-05 13:51:25 -0400

    added Final

---
+++ Final/contact.php
@@ -0,0 +1,27 @@
+<?php 
+$name = $_Post['name'];
+$visitor_email = $_Post['email'];
+$message = $_POST['message'];
+
+$email_from = 'nicolejpineda@gmail.com'
+
+$email_subject= "New Form Submission"
+
+$email_body = "User Name: $name.\n".
+"User Message: $message.\n";
+
+
+$to = "nicole92099@gmail.com";
+
+$headers = "From" $email_from \r\n";
+
+
+$headers = "Reply-To" $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
+?>
+
+
